c07083bcaf5118d091f05142a604d5c55226188e
Drew Wicke <drewwicke@gmail.com>
2021-08-31 16:17:35 -0400

    simple start to the ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import HomepageImage from './components/HomepageImage'
+import PrimarySearchAppBar from './components/MyAppBar';
 import './App.css';
+import AutoGrid from './components/RecipesPage';
 
 function App() {
   return (
     <div className="App">
+      <PrimarySearchAppBar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <HomepageImage />
+        <AutoGrid />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
